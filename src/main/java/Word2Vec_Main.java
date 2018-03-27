335b5d9f9aac30fee1004f774227cd0db699a742
Tomás Mark Flynn <tomasflynn@Tomass-MacBook-Pro.local>
2018-03-27 14:25:41 +0100

    First commit

---
+++ src/main/java/Word2Vec_Main.java
@@ -0,0 +1,58 @@
+
+
+
+import org.datavec.api.util.ClassPathResource;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.sentenceiterator.BasicLineIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collection;
+
+
+
+public class Word2Vec_Main {
+
+    private static Logger log = LoggerFactory.getLogger(Word2Vec_Main.class);
+
+    public static void main(String[] args) throws Exception {
+
+        // Gets Path to Text file
+        String filePath = new ClassPathResource("gameofthrones.txt").getFile().getAbsolutePath();
+
+        log.info("Load & Vectorize Sentences....");
+        // Strip white space before and after for each line
+        SentenceIterator iter = new BasicLineIterator(filePath);
+        // Split on white spaces in the line to get words
+        TokenizerFactory t = new DefaultTokenizerFactory();
+
+        /*
+            CommonPreprocessor gets rid of all number, punctuation... as the ANN can only
+            process tokens that are strings
+         */
+        t.setTokenPreProcessor(new CommonPreprocessor());
+
+
+        log.info("Building model....");
+        Word2Vec vec = new Word2Vec.Builder()
+                .minWordFrequency(5)
+                .iterations(1)
+                .layerSize(100)
+                .seed(42)
+                .windowSize(5)
+                .iterate(iter)
+                .tokenizerFactory(t)
+                .build();
+
+        log.info("Fitting Word2Vec model....");
+        vec.fit();
+
+
+
+    }
+}
